eec0bcb31253c49dad7616ab031a56f0ee5589ef
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-10-24 16:42:15 +0300

    update

--- setting.py
+++ setting.py
@@ -73,7 +73,15 @@ STATICFILES_DIRS = [
 ]
 
 # تسجيل ملف ال Media
+# urls 
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('lms_app.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+# setting
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 

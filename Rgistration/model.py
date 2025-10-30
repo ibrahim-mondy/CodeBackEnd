25b67e56372fb7d25da539b10895296c7e55609e
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-10-30 22:15:06 +0300

    views done

--- Rgistration/model.py
+++ Rgistration/model.py
@@ -22,3 +22,17 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ("username", "email", "bio")
+# متى تستخدمه:
+
+# لما تستخدم CustomUser وعايز تعرض الحقول الإضافية في صفحة التسجيل.
+
+# views.py
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import CustomUserCreationForm  # لو عندك فورم مخصص
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm  # أو UserCreationForm لو بدون فورم مخصص
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
+

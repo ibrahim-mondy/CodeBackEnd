51207de88821436a00c074404199833ee1434cd5
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-10-30 22:15:29 +0300

    urls done

--- Rgistration/model.py
+++ Rgistration/model.py
@@ -36,3 +36,19 @@ class SignUpView(CreateView):
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
+# urls.py
+from django.urls import path, include
+from django.views.generic import TemplateView
+from .views import SignUpView
+
+urlpatterns = [
+    # تسجيل الدخول / الخروج / تغيير الباسورد الجاهزة
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # صفحة البروفايل بعد تسجيل الدخول
+    path('accounts/profile/', TemplateView.as_view(template_name='accounts/profile.html'), name='profile'),
+
+    # صفحة التسجيل
+    path('signup/', SignUpView.as_view(), name='signup'),
+]
+

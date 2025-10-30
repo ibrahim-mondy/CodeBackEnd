be9ef1d93ece2dfbf2575840d6650a53ff639d2b
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-10-30 22:13:24 +0300

    form is done

--- Rgistration/model.py
+++ Rgistration/model.py
@@ -12,3 +12,13 @@ class CustomUser(AbstractUser):
 
 # في settings.py
 AUTH_USER_MODEL = 'myapp.CustomUser'
+
+# forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ("username", "email", "bio")
